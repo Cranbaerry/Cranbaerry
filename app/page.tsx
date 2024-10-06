6d4f23140e22da3a723440cf4fee2fe3099d1708
Naufal Hardiansyah <naufalh39@gmail.com>
2024-10-06 22:59:46 +0000

    adj: remove resume

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import { Github, MessageCircle, FileText, Linkedin, Mail } from 'lucide-react';
+import { Github, MessageCircle, Linkedin, Mail } from 'lucide-react';
 import { MinimalistProgrammerLinktree, LinkItem } from "@/components/minimalist-programmer-linktree";
 export default function Home() {
     const name = "Naufal Hardiansyah";
     const role = "Full Stack Developer | ML Enthusiast";
     const pictureUrl = '/assets/profile.jpg';
-    const resumePdf = '/assets/Naufal Hardiansyah Resume.pdf';
 
     const links: LinkItem[] = [
-        { name: 'Resume', icon: <FileText />, url: resumePdf },
         { name: 'LinkedIn', icon: <Linkedin />, url: 'https://www.linkedin.com/in/naufal-h/' },
         { name: 'GitHub', icon: <Github />, url: 'https://github.com/Cranbaerry' },
         { name: 'WhatsApp', icon: <MessageCircle />, url: 'https://wa.me/6287766250158' },
